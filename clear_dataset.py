bceadbdbfd7551d7e6f980c30e13cbdb0caaab03
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2015-11-16 18:00:04 -0500

    Smarter filtering on clear_datasets

--- clear_dataset.py
+++ clear_dataset.py
@@ -22,13 +22,30 @@ def place_done_callback(place, place_response):
 
     step += 1
 
-def main(config, delete=True):
+def place_matches_filters(place, included_attributes=[], restricted_attributes=[], eq_attribute_values={}, ne_attribute_values={}):
+    for attr in included_attributes:
+        if attr not in place:
+            return False
+    for attr in restricted_attributes:
+        if attr in place:
+            return False
+    for attr, val in eq_attribute_values.items():
+        if place.get(attr, None) != val:
+            return False
+    for attr, val in ne_attribute_values.items():
+        if place.get(attr, None) == val:
+            return False
+    return True
+
+def main(config, delete=True, included_attributes=[], restricted_attributes=[], eq_attribute_values={}, ne_attribute_values={}):
     tool = ShareaboutsTool(config['host'])
     all_places = [
         place for place in
         tool.get_places(config['owner'], config['dataset'])
         #...put a condition here to filter the places, if desired...
-        if 'Soc_V' in place
+        if place_matches_filters(
+            place, included_attributes, restricted_attributes,
+            eq_attribute_values, ne_attribute_values)
     ]
 
     print('Deleting the %s places...' % (len(all_places),))
@@ -44,8 +61,17 @@ if __name__ == '__main__':
     parser = ArgumentParser(description='Remove all places from a dataset.')
     parser.add_argument('configuration', type=str, help='The configuration file name')
     parser.add_argument('--test', '--no-delete', dest='delete', action='store_false', help='Actually delete the places?')
+    parser.add_argument('--has-attr', nargs='*', help='Delete items that have the attribute(s)')
+    parser.add_argument('--wo-attr', nargs='*', help='Delete items that do not have the attribute(s)')
+    parser.add_argument('--attr-is', nargs='*', help='Delete items that have the specific attribute values')
+    parser.add_argument('--attr-not', nargs='*', help='Delete items that do not have the specific attribute values')
 
     args = parser.parse_args()
     config = json.load(open(args.configuration))
 
-    main(config, delete=args.delete)
+    main(
+        config, delete=args.delete,
+        included_attributes=args.has_attr, restricted_attributes=args.wo_attr,
+        eq_attribute_values=dict([item.split('=') for item in args.attr_is]),
+        ne_attribute_values=dict([item.split('=') for item in args.attr_not]),
+    )
